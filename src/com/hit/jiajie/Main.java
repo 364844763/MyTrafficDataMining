5f5319538c955d3a15ecd7e7e54bf3ba3db6f6b8
364844763 <364844763@qq.com>
2015-11-05 21:01:58 +0800

    第2次处理数据

--- src/com/hit/jiajie/Main.java
+++ src/com/hit/jiajie/Main.java
@@ -1,7 +1,9 @@
 package com.hit.jiajie;
 
 import com.hit.jiajie.bean.LineInformation;
+import com.hit.jiajie.reader.ClassifyByDay;
 import com.hit.jiajie.reader.RouteClassification;
+import com.hit.jiajie.writer.ClassifyByDayWriter;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -12,8 +14,13 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        ClassifyByDay classifyByDay=new ClassifyByDay();
+        Map map=classifyByDay.read("E:\\天池大赛\\Part1-数据\\gb_train_classify_by_day.CSV");
+        ClassifyByDayWriter writer=new ClassifyByDayWriter();
+        writer.write(map,"E:\\天池大赛\\Part1-数据\\gb_train_classify_by_hour.CSV");
 	// write your code here
-        RouteClassification routeClassification=new RouteClassification();
+       /* RouteClassification routeClassification=new RouteClassification();
         Map map=routeClassification.read("E:\\天池大赛\\Part1-数据\\gd_train_data.txt");
         Iterator iter = map.entrySet().iterator();
         FileWriter fw = new FileWriter("E:\\天池大赛\\Part1-数据\\gb_train_classify_by_day.txt", true);
@@ -27,6 +34,6 @@ public class Main {
             bw.flush();
         }
         bw.close();
-        fw.close();
+        fw.close();*/
     }
 }
